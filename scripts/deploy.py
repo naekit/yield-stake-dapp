3416cf3d9fb89570e7978afa81dbd6419aee9c0e
Mykyta <mykyta.onishchuck@code.berlin>
2022-02-14 18:53:17 +0100

    deploy token and token farm, begin to import mockcontracts for testing environment

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,7 +1,30 @@
-from scripts.helpful_scripts import get_account
+from scripts.helpful_scripts import get_account, get_contract
+from brownie import YietToken, TokenFarm, network, config
+from web3 import Web3
+
+KEPT_BALANCE = Web3.toWei(100, "ether")
+
 
 def deploy_token_farm_and_dapp_token():
     account = get_account()
+    yiet_token = YietToken.deploy({"from": account})
+    token_farm = TokenFarm.deploy(
+        yiet_token.address,
+        {"from": account},
+        publis_source=config["networks"][network.show_active()].get("verify", False),
+    )
+    tx = yiet_token.transfer(
+        token_farm.address, yiet_token.totalSupply() - KEPT_BALANCE, {"from": account}
+    )
+    tx.wait(1)
+    # yiet_token, weth_token, fau_token/dai
+    weth_token = get_contract("weth_token")
+    fau_token = get_contract("fau_token")
+    
+    add_allowed_tokens(token_farm,)
+    
+def add_allowed_tokens(token_farm, dict_of_allowed_tokens, account)
+    pass
 
 def main():
     deploy_token_farm_and_dapp_token()
--- scripts/helpful_scripts.py
+++ scripts/helpful_scripts.py
@@ -29,19 +29,6 @@ def get_account(index=None, id=None):
     return accounts.add(config["wallets"]["from_key"])
 
 
-def get_account(index=None, id=None):
-    if index:
-        return accounts[index]
-    if (
-        network.show_active() in LOCAL_BLOCKCHAIN_ENVIRONMENTS
-        or network.show_active() in FORKED_LOCAL_ENVIRONMENTS
-    ):
-        return accounts[0]
-    if id:
-        return accounts.load(id)
-    return accounts.add(config["wallets"]["from_key"])
-
-
 contract_to_mock = {
     "vrf_coordinator": VRFCoordinatorMock,
     "link_token": LinkToken,
