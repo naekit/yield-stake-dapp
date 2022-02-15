d4a926693d9dbf50fa861c0d9ad134de57486609
Mykyta <mykyta.onishchuck@code.berlin>
2022-02-15 18:00:44 +0100

    get all addresses and chain to identiyfy where we are and get balances

--- front_end/src/components/main.tsx
+++ front_end/src/components/main.tsx
@@ -1,5 +1,8 @@
 import { useEthers } from "@usedapp/core"
 import helperConfig from "../helper-config.json"
+import networkMapping from "../chain-info/deployments/map.json"
+import { constants } from "ethers"
+import brownieConfig from "../brownie-config.json"
 
 export const Main = () => {
     // show token amount from wallet
@@ -11,6 +14,9 @@ export const Main = () => {
     // send build folder (yiet token and mock addresses)
     const { chainId } = useEthers()
     const networkName = chainId ? helperConfig[chainId] : "dev"
-    // const yietTokenAddress ???
-    return (<div>Hi!</div>)
+
+    const yietTokenAddress = chainId ? networkMapping[String(chainId)]["YietToken"][0] : constants.AddressZero// look into mapping : 000
+    const wethTokenAddress = chainId ? brownieConfig["networks"][networkName]["weth_token"] : constants.AddressZero // brownie config
+    const fauTokenAddress = chainId ? brownieConfig["networks"][networkName]["fau_token"] : constants.AddressZero
+    return (<div><h1>I Am Main!</h1></div>)
 }
