5b5a43d5b981f1feb2141441697d533a558a487a
Mykyta <mykyta.onishchuck@code.berlin>
2022-02-15 14:22:57 +0100

    test stake tokens in order to test issue tokens function, add initial value var and amount_staked fixture for pytest

--- tests/unit/test_token_farm.py
+++ tests/unit/test_token_farm.py
@@ -1,5 +1,6 @@
 from brownie import network, exceptions
 from scripts.helpful_scripts import (
+    INITIAL_PRICE_FEED_VALUE,
     LOCAL_BLOCKCHAIN_ENVIRONMENTS,
     get_account,
     get_contract,
@@ -26,3 +27,40 @@ def test_set_price_feed_contract():
         token_farm.setPriceFeedContract(
             yiet_token.address, price_feed_address, {"from": non_owner}
         )
+
+
+def test_stake_tokens(amount_staked):
+    # Arrange
+    if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+        pytest.skip("Only for local testing")
+    account = get_account()
+    token_farm, yiet_token = deploy_token_farm_and_yiet_token()
+    # Act
+    yiet_token.approve(token_farm.address, amount_staked, {"from": account})
+    token_farm.stakeTokens(amount_staked, yiet_token.address, {"from": account})
+    # Assert
+    assert (
+        token_farm.stakingBalance(yiet_token.address, account.address) == amount_staked
+    )
+    assert token_farm.uniqueTokensStaked(account.address) == 1
+    assert token_farm.stakers(0) == account.address
+    return token_farm, yiet_token
+
+
+def test_issue_tokens(amount_staked):
+    # Arrange
+    if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+        pytest.skip("Only for local testing")
+    account = get_account()
+    token_farm, yiet_token = test_stake_tokens(amount_staked)
+    starting_balance = yiet_token.balanceOf(account.address)
+    # Act
+    token_farm.issueTokens({"from": account})
+    # Assert
+    # staking 1 yiet_token == in price to 1 eth
+    # initial pf value of MOCK is 2000000000000000000000 / price of ETH
+    # so we expect to get 2000 yiet tokens in reward
+    assert (
+        yiet_token.balanceOf(account.address)
+        == starting_balance + INITIAL_PRICE_FEED_VALUE
+    )
